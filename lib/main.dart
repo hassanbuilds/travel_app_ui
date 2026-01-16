35ad3e33828967a73aba77d27ea5bb70193679c0
Muhammad Hassan <mhassan.codes@gmail.com>
2026-01-16 16:16:09 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:travel_app/provider/travel_provider.dart';
-
 import 'screens/home_screen.dart';
 
 void main() {
